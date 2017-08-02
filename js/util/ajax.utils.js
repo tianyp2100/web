a92881b31637291e717fa0a60ed017bb35f2e807
tianyongping <tianyp@hxwisec.com>
2017-08-02 08:56:48 +0800

    修改jsonp和ignore文件加入

--- js/util/ajax.utils.js
+++ js/util/ajax.utils.js
@@ -4,29 +4,30 @@
  * @time 2017-07-27
  * 参照：http://www.w3school.com.cn/jquery/ajax_ajax.asp
  */
+
 /**
- * exeFunc 请求封装方法
+ * exeFunc json请求封装方法
  * @param url 请求地址
  * @param type 请求类型 GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS,TRACE
  * @param async  请求发起类型 同步false,异步true(默认)
  * @param timeout  请求超时时间（毫秒），默认1分钟
  * @param dataType 预期服务器返回的数据类型 xml,html,script,json,sonp,text
- * @param data 服务器返回数据
+ * @param reqData 服务器请求数据
  * @param succFunc 请求成功回调方法
  * @param errFunc 请求失败回调方法
  */
-function exeFunc(url, type, async, timeout, dataType, data, succFunc, errFunc) {
+function exeFunc(url, type, async, timeout, dataType, reqData, succFunc, errFunc) {
     type = (isNoData(type)) ? "POST" : type;
     async = isNoData(async) ? true : async;
     dataType = isNoData(dataType) ? "json" : dataType;
-    data = isNoData(data) ? {} : data;
+    reqData = isNoData(reqData) ? {} : reqData;
     timeout = !isNumber() ? 60000 : timeout;
     $.ajax({
         type: type,
         url: url,
         async: async,
         timeout: timeout,
-        data: data,
+        data: reqData,
         dataType: dataType,
         success: function (data) {
             succFunc(data);
@@ -36,19 +37,30 @@ function exeFunc(url, type, async, timeout, dataType, data, succFunc, errFunc) {
         }
     });
 }
-function exeFuncJSONP(url, type, async, timeout, data, succFunc, errFunc) {
+
+/**
+ * exeFuncJsonP jsonp请求封装方法
+ * @param url 请求地址
+ * @param type 请求类型 GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS,TRACE
+ * @param async  请求发起类型 同步false,异步true(默认)
+ * @param timeout  请求超时时间（毫秒），默认1分钟
+ * @param reqData 服务器请求数据
+ * @param succFunc 请求成功回调方法
+ * @param errFunc 请求失败回调方法
+ */
+function exeFuncJsonP(url, type, async, timeout, reqData, succFunc, errFunc) {
     type = (isNoData(type)) ? "POST" : type;
     async = isNoData(async) ? true : async;
-    data = isNoData(data) ? {} : data;
+    reqData = isNoData(reqData) ? {} : reqData;
     timeout = !isNumber() ? 60000 : timeout;
     $.ajax({
         type: type,
         url: url,
         async: async,
         timeout: timeout,
-        data: data,
-        dataType: "jsonp",
-        jsonp: "callback",
+        data: reqData,
+        dataType: "jsonp", //返回JSONP格式的数据，此值固定
+        jsonp: "callback", //回调函数的名字，此值固定
         success: function (data) {
             succFunc(data);
         },
@@ -58,17 +70,43 @@ function exeFuncJSONP(url, type, async, timeout, data, succFunc, errFunc) {
     });
 }
 /**
- * ajaxFunc 请求封装方法
- * */
-function ajaxFunc(url, data, succFunc) {
-    exeFunc(url, "POST", true, 30000, "json", data, succFunc, defErrFunc);
+ * ajaxFunc json请求封装方法
+ * @param url 请求地址
+ * @param reqData 服务器请求数据
+ * @param succFunc 请求成功回调方法
+ */
+function ajaxFunc(url, reqData, succFunc) {
+    exeFunc(url, "POST", true, 30000, "json", reqData, succFunc, defErrFunc);
+}
+/**
+ * ajaxFunc json请求封装方法
+ * @param url 请求地址
+ * @param reqData 服务器请求数据
+ * @param succFunc 请求成功回调方法
+ * @param errFunc 请求失败回调方法
+ */
+function ajaxFunc(url, reqData, succFunc, errFunc) {
+    exeFunc(url, "POST", true, 30000, "json", reqData, succFunc, errFunc);
 }
 
 /**
- * ajaxFunc 请求封装方法 jsonp
- * */
-function ajaxFuncJSONP(url, data, succFunc) {
-    exeFuncJSONP(url, "POST", true, 30000, data, succFunc, defErrFunc);
+ * ajaxFunc jsonp请求封装方法
+ * @param url 请求地址
+ * @param reqData 服务器请求数据
+ * @param succFunc 请求成功回调方法
+ */
+function ajaxFuncJsonP(url, reqData, succFunc) {
+    exeFuncJsonP(url, "POST", true, 30000, reqData, succFunc, defErrFunc);
+}
+/**
+ * ajaxFunc jsonp请求封装方法
+ * @param url 请求地址
+ * @param reqData 服务器请求数据
+ * @param succFunc 请求成功回调方法
+ * @param errFunc 请求失败回调方法
+ */
+function ajaxFuncJsonP(url, data, succFunc, errFunc) {
+    exeFuncJsonP(url, "POST", true, 30000, data, succFunc, errFunc);
 }
 
 function defErrFunc() {
